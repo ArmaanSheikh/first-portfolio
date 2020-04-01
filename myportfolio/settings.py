65fa8672fec6c48b3d1b682e277cb35c6b45d960
ArmaanSheikh <you@example.com>
2020-04-01 13:35:50 +0000

    My Changes

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0^7f-b_%8f8np&(fiw(%tb5f9dmy5k6up8%#2%b_cx9_%^ig2r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['armaansheikh.pythonanywhere.com']
 
 
 # Application definition
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'portfolio',
     'blog',
 ]
@@ -120,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'static')
 ]
